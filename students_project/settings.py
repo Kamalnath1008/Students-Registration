5e66ce8b8ffcc189b9b2b0427f8bcf6944dd8c1b
Kamalnath S <154576809+Kamalnath1008@users.noreply.github.com>
2024-03-22 00:07:24 +0530

    Update settings.py

--- students_project/settings.py
+++ students_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--+6e$9j5f$&%u7^idxhir%0(&%q8r&h+ujcdxyi(b%t2oyt-tn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
